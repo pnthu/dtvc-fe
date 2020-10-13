e9863ceb8c77567ef6f0597d2ccd13160e1d0083
Pnthu <pnthu99@gmail.com>
2020-10-13 23:29:11 +0700

    draw line

---
+++ src/pages/LoginScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+class LoginScreen extends React.Component {
+    render() {
+        return (
+            <div>Login</div>
+        )
+    }
+}
+
+export default LoginScreen;
