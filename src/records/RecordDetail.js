6c37a74cba432c3319fcd44054c1eeec8ceee477
Pnthu <pnthu99@gmail.com>
2020-10-28 23:32:50 +0700

    record mgmt + detail + login

--- src/records/RecordDetail.js
+++ src/records/RecordDetail.js
@@ -1,8 +1,51 @@
 import * as React from "react";
+import { Modal, Button } from "antd";
+import "./RecordDetail.css";
+
+const actions = {
+  Pending: (
+    <>
+      <Button type="default">Reject</Button>
+      <Button type="primary">Approve</Button>
+    </>
+  ),
+  Approved: <Button type="primary">Export to PDF</Button>,
+  Rejected: null,
+};
 
 class RecordDetail extends React.Component {
   render() {
-    return <div>Record Detail</div>;
+    console.log("data", this.props);
+    return (
+      <Modal visible={this.props.visible} footer={null} maskClosable>
+        <h3 className="title">VIOLATION RECORD</h3>
+        <div className="container">
+          <div>
+            <p className="label">Time</p>
+            <p className="label">Location</p>
+            <p className="label">Violation Type</p>
+            <p className="label">License Plate Number</p>
+            <p className="label">Status</p>
+          </div>
+          <div className="modalRight">
+            <p>{this.props.data.time}</p>
+            <p>{this.props.data.location}</p>
+            <p>{this.props.data.type}</p>
+            <p>{this.props.data.licensePlateNumber}</p>
+            <p>{this.props.data.status}</p>
+          </div>
+        </div>
+        <div
+          style={{
+            width: "100%",
+            height: "300px",
+            margin: "36px 0px 24px 0px",
+            backgroundColor: "gray",
+          }}
+        ></div>
+        <div>{actions[this.props.data.status]}</div>
+      </Modal>
+    );
   }
 }
 
--- src/user-management/Login.js
+++ src/user-management/Login.js
@@ -1,10 +1,16 @@
 import * as React from "react";
-import { Button } from "antd";
+import "antd/dist/antd.css";
+import { Button, Input, Form } from "antd";
+import "./Login.css";
 
 class Login extends React.Component {
-  componentDidMount = () => {
-    window.sessionStorage.removeItem("role");
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+    };
+  }
 
   login = () => {
     window.sessionStorage.setItem("role", "moderator");
@@ -14,8 +20,34 @@ class Login extends React.Component {
   render() {
     console.log("props", this.props);
     return (
-      <div>
-        <Button onClick={this.login}>Login</Button>
+      <div className="loginBackground">
+        <h1 className="loginTitle">Traffic Violation Detection System</h1>
+        <img src={require("../logo.svg")} alt="logo" className="logo" />
+
+        <Form name="basic" style={{ width: "50%" }}>
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Please input your username" }]}
+          >
+            <Input placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your password" }]}
+          >
+            <Input.Password placeholder="Password" />
+          </Form.Item>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              onClick={this.login}
+              className="login"
+            >
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     );
   }
